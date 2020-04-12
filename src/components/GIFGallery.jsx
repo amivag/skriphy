5b53b30d35567152ee550106fdc86f8655c8884e
Evan (amivag) <amivag@gmail.com>
2020-04-12 11:19:31 +0300

    Refactored copy to clipboard code

--- src/components/GIFGallery.jsx
+++ src/components/GIFGallery.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { copyTextToClipboard } from "../libs/clipboard.js";
+
 /**
  * Extract useful properties from a GIF image object (GIPHY API).
  * // https://developers.giphy.com/docs/api/schema
@@ -56,7 +58,7 @@ const GIFImageItem = ({ itemId, urlGIFPreview, removeItemById }) => {
           type="button"
           className="btn copy-link"
           onClick={() => {
-            copyToClipboard(urlGIFPreview);
+            copyTextToClipboard(urlGIFPreview);
           }}
         >
           Copy Link
@@ -79,23 +81,3 @@ GIFImageItem.propTypes = {
   urlGIFPreview: PropTypes.string,
   removeItemById: PropTypes.func,
 };
-
-/**
- * Utility function
- * Sourced from...
- * https://stackoverflow.com/questions/33855641/copy-output-of-a-javascript-variable-to-the-clipboard
- * @param {*} text
- */
-function copyToClipboard(text) {
-  var dummy = document.createElement("textarea");
-  // to avoid breaking orgain page when copying more words
-  // cant copy when adding below this code
-  // dummy.style.display = 'none'
-  document.body.appendChild(dummy);
-  //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
-  dummy.value = text;
-  dummy.select();
-  document.execCommand("copy");
-  document.body.removeChild(dummy);
-  //console.log(`Copied to clipboard ${text}`);
-}
